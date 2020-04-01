03fc6e84008ea74057f5fc54562f46672f83cdae
lesfernandes <lemayara16@gmail.com>
2020-04-01 13:05:31 -0300

    Organização Básica de um Código

---
+++ fundamentos-js/organizacao.js
@@ -0,0 +1,11 @@
+// JavaScript é organizado por sentenças de códigos que podem ou não terminar com ponto e v´rigula
+console.log("Sentença de código") //Padrão do curso - sem ponto e vírgula
+
+// E também por blocos de códigos delimitados por chaves
+{
+    {
+        console.log("Olá")
+        console.log("Mundo!") 
+
+    }
+}
