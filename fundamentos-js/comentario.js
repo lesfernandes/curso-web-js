fd469d8bd7f0fd687eccfe1d0e60eacf155faf72
lesfernandes <lemayara16@gmail.com>
2020-04-01 13:25:49 -0300

    Comentários de Código

---
+++ fundamentos-js/comentario.js
@@ -0,0 +1,19 @@
+// Comentário de uma linha
+console.log("Linha 1")
+
+/*
+    Comentário de 
+    múltiplas linhas
+*/
+console.log("Linha 2")
+
+/*
+    * Comentário de 
+    * Múltiplas linhas
+    * Com um estilo
+*/
+console.log("Linha 3")
+
+// Dê preferência à um código legível
+// Faça comentários relevantes e evite comentários óbvios
+// Comente tudo o que tiver necessidade de comentar (em cursos)
