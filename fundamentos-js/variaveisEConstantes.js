d5fb3f6d8fe05a88e342a09ef1b9ce60fa777231
lesfernandes <lemayara16@gmail.com>
2020-04-01 13:34:37 -0300

    O Básico de Var, Let e Const

---
+++ fundamentos-js/variaveisEConstantes.js
@@ -0,0 +1,21 @@
+// Criação de variáveis
+var a = 3
+
+// Uso mais moderno de criar variáveis
+let b = 4
+
+var a = 30 //Utilizando var, podemos redeclarar uma variável no mesmo escopo
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+// Criação de constantes
+const c = 5
+
+// c = 50
+console.log(c)
