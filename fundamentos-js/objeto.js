17ad11ade4958db72ff26b488bc09bcebbd50442
lesfernandes <lemayara16@gmail.com>
2020-04-02 13:43:59 -0300

    Tipos em JavaScript: Object

---
+++ fundamentos-js/objeto.js
@@ -0,0 +1,12 @@
+//Objeto em  JavaScript é uma coleção de chave:valor
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 49998.98
+prod1['Desconto Legal'] = 0.40 //Evitar atributos com espaço
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+console.log(prod2)
