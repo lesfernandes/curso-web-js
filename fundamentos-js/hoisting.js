50ca2d52cec96a893632adaf10edf7ed05560cd4
lesfernandes <lemayara16@gmail.com>
2020-04-03 16:55:36 -0300

    Entendendo o Hoisting

---
+++ fundamentos-js/hoisting.js
@@ -0,0 +1,16 @@
+// Içamento -> joga para cima
+/* 
+    var a 
+    console.log('a =', a)
+    a = 2
+    console.log('a =', a)
+*/
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+
+// Usando let, o içamento não é permitido
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
