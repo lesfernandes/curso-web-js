3558e8b6bf78b407fbf1cf6f0348892a6e9e615a
Letícia Fernandes <lemayara16@gmail.com>
2020-11-04 12:28:31 -0300

    Call & Apply

---
+++ funcao/callApply.js
@@ -0,0 +1,27 @@
+function getPreco(imposto = 0, moeda = 'R$') {
+    return moeda + (this.preco * (1 - this.desc) * (1 + imposto))
+}
+
+const produto = {
+    nome: 'Notebook',
+    preco: 4589,
+    desc: 0.15, 
+    getPreco
+}
+
+global.preco = 20
+global.desc = 0.1
+console.log(getPreco())
+
+console.log(produto.getPreco())
+
+const carro = {preco: 49990, desc: 0.20}
+
+console.log(getPreco.call(carro))
+console.log(getPreco.apply(carro))
+
+// Call -> parametros diretamente na função
+console.log(getPreco.call(carro, 0.17, '$'))
+
+// Apply -> parametros dentro de um array
+console.log(getPreco.apply(global, [0.17, '$']))
